eb45bb73192b2863746b778392584783004fdac0
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2019-10-31 15:09:07 +0100

    commit

---
+++ kfdgjmsj/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace kfdgjmsj
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }//declaraties
+        ushort leeftijd;
+
+        //input
+        Console.WriteLine("wat is uw huidige leeftijd?: ");
+            leeftijd = ushort.Parse(Console.ReadLine());
+
+            //bereken
+            if (leeftijd > 18)
+            {
+                Console.WriteLine("je bent oud genoeg om deel te nemen aan de loterij");
+            }
+            else
+            {
+                Console.WriteLine("om deel te nemen moet je minimum 18 jaar oud zijn");
+            }
+
+//toon
+
+
+//wachten
+Console.WriteLine();
+            Console.WriteLine("druk op enter om af te sluiten");
+            Console.ReadKey();
+    }
+}
